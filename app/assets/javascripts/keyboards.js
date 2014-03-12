223395bc80d678ae7dfeb51a575d32933f2c3e7d
jayrogers889csa <jayrogers889@gmail.com>
2014-03-12 15:51:27 -0500

    Continuing to hook up the keys

--- app/assets/javascripts/keyboards.js
+++ app/assets/javascripts/keyboards.js
@@ -5,18 +5,18 @@ $(document).ready(function() {
 
     oscillator = context.createOscillator();
 
-    var keycode_one = 65; //a
-    var keycode_two = 87; //w
-    var keycode_three = 68; //d
-    var keycode_four = 82; //r
-    var keycode_five = 82; //g
-    var keycode_six = 82; //h
-    var keycode_seven = 82; //u
-    var keycode_eight = 82; //k
-    var keycode_nine = 82; //r
-    var keycode_ten = 82; //r
-    var keycode_eleven = 82; //r
-    var keycode_twelve = 82; //r
+    var keycode_one = 16; //shift
+    var keycode_two = 81; //q
+    var keycode_three = 83; //s
+    var keycode_four = 69; //e
+    var keycode_five = 70; //f
+    var keycode_six = 71; //g
+    var keycode_seven = 89; //y
+    var keycode_eight = 74; //j
+    var keycode_nine = 73; //i
+    var keycode_ten = 76; //l
+    var keycode_eleven = 80; //p
+    var keycode_twelve = 222; //2
     var keycode_thirteen = 82; //r
 
 
@@ -31,6 +31,24 @@ $(document).ready(function() {
             playNote("D", "pitch_slider_three", "wave_three", keycode_three);
         } else if (event.keyCode == keycode_four) {
             playNote("D#", "pitch_slider_four", "wave_four", keycode_four);
+        } else if (event.keyCode == keycode_five) {
+            playNote("E", "pitch_slider_two", "wave_two", keycode_two);
+        } else if (event.keyCode == keycode_six) {
+            playNote("F", "pitch_slider_two", "wave_two", keycode_two);
+        } else if (event.keyCode == keycode_seven) {
+            playNote("F#", "pitch_slider_two", "wave_two", keycode_two);
+        } else if (event.keyCode == keycode_eight) {
+            playNote("G", "pitch_slider_two", "wave_two", keycode_two);
+        } else if (event.keyCode == keycode_nine) {
+            playNote("G#", "pitch_slider_two", "wave_two", keycode_two);
+        } else if (event.keyCode == keycode_ten) {
+            playNote("C#", "pitch_slider_two", "wave_two", keycode_two);
+        } else if (event.keyCode == keycode_eleven) {
+            playNote("C#", "pitch_slider_two", "wave_two", keycode_two);
+        } else if (event.keyCode == keycode_twelve) {
+            playNote("C#", "pitch_slider_two", "wave_two", keycode_two);
+        } else if (event.keyCode == keycode_thirteen) {
+            playNote("C#", "pitch_slider_two", "wave_two", keycode_two);
         }
     });
 
