95371645f563e8b6bcd9f258a574fe79ee227154
dan <dandydanny@gmail.com>
2014-03-14 12:10:45 -0500

    theres some error somewhere which needs to be fixed

--- app/assets/javascripts/keyboards.js
+++ app/assets/javascripts/keyboards.js
@@ -1,10 +1,10 @@
 function Key(keycode, html_id)
 {
     this.oscillator = context.createOscillator();
-    this.panners = context.createPanner();
-    this.delays = context.createDelayNode();
-    this.filters = context.createBiquadFilter();
-    this.volumes = context.createGainNode();
+    this.panner = context.createPanner();
+    this.delay = context.createDelayNode();
+    this.filter = context.createBiquadFilter();
+    this.volume = context.createGainNode();
     this.keycode = keycode;
     this.html_id = html_id;
     this.keyBeingPlayed = false;
@@ -12,6 +12,7 @@ function Key(keycode, html_id)
 
     var buildIDsForHTML = function()   // Only visible inside Restaurant()
     {
+        console.log('here');
         this.frequency_id = "frequency_" + this.html_id;
         this.wave_id = "wave_" + this.html_id;
         this.panner_id = "panner_" + this.html_id;
@@ -21,52 +22,52 @@ function Key(keycode, html_id)
     }
 }
 
-Key.prototype.doesKeyMatchKeyEvent = function(keyFromEvent)
-{
-    if (this.keycode == keyFromEvent)
-        return true;
+// Key.prototype.doesKeyMatchKeyEvent = function(keyFromEvent)
+// {
+//     if (this.keycode == keyFromEvent)
+//         return true;
 
-    return false;
-}
+//     return false;
+// }
 
-Key.prototype.isKeyPlaying = fucntion()
-{
-    return this.keyBeingPlayed;
-}
+// Key.prototype.isKeyPlaying = function()
+// {
+//     return this.keyBeingPlayed;
+// }
 
-Key.prototype.playNote = function()
-{
-    this.keyBeingPlayed = true;
+// Key.prototype.playNote = function()
+// {
+//     this.keyBeingPlayed = true;
 
-    var osc = document.getElementById(this.html_id);
-    $(osc).addClass('cool-border');
+//     var osc = document.getElementById(this.html_id);
+//     $(osc).addClass('cool-border');
 
-    var oscPitch = document.getElementById(this.frequency_id).value;
-    var panX = document.getElementById(this.panner_id).value;
-    this.oscillator.type = parseInt(document.getElementById(this.wave_type_id).value);
-    this.oscillator.frequency.value = oscPitch;
-    this.volume.gain.value = document.getElementById(this.volume_id).value;
-    this.filter.type = filter.BANDPASS
-    this.filter.detune.value = document.getElementById(filter_id).value;
-    this.panner.setPosition(panX, 0, 0);
+//     var oscPitch = document.getElementById(this.frequency_id).value;
+//     var panX = document.getElementById(this.panner_id).value;
+//     this.oscillator.type = parseInt(document.getElementById(this.wave_type_id).value);
+//     this.oscillator.frequency.value = oscPitch;
+//     this.volume.gain.value = document.getElementById(this.volume_id).value;
+//     this.filter.type = filter.BANDPASS
+//     this.filter.detune.value = document.getElementById(filter_id).value;
+//     this.panner.setPosition(panX, 0, 0);
 
-    this.oscillator.connect(this.panner);
-    this.panner.connect(this.filter);
-    this.filter.connect(this.volume);
-    this.volume.connect(context.destination);
+//     this.oscillator.connect(this.panner);
+//     this.panner.connect(this.filter);
+//     this.filter.connect(this.volume);
+//     this.volume.connect(context.destination);
 
-    this.oscillator.noteOn(0);
-}
+//     this.oscillator.noteOn(0);
+// }
 
-Key.prototype.stopNote() = function()
-{
-    this.keyBeingPlayed = false;
+// Key.prototype.stopNote() = function()
+// {
+//     this.keyBeingPlayed = false;
 
-    var osc = document.getElementById(html_id)
-    $(osc).removeClass('cool-border');
-    oscillator.noteOff(0);
-    oscillator = context.createOscillator();
-}
+//     var osc = document.getElementById(html_id)
+//     $(osc).removeClass('cool-border');
+//     oscillator.noteOff(0);
+//     oscillator = context.createOscillator();
+// }
 
 $(document).ready(function()
 {
@@ -86,24 +87,23 @@ $(document).ready(function()
     keys.push(new Key(80, 11));//p
     keys.push(new Key(222, 12));//'
     keys.push(new Key(221, 13));//]
-    average = 0;
 
-    $(document).on("keydown", function(event)
-    {
-        for (var i = 0; i < keys.length; i++)
-        {
-            if ((keys[i].doesKeyMatchKeyEvent(event.keyCode) && (keys[i].isKeyPlaying() != true))
-            {
-                keys[i].playNote();
-            }
-        }
-    });
+    // $(document).on("keydown", function(event)
+    // {
+    //     for (var i = 0; i < keys.length; i++)
+    //     {
+    //         if ((keys[i].doesKeyMatchKeyEvent(event.keyCode) && (keys[i].isKeyPlaying() != true))
+    //         {
+    //             keys[i].playNote();
+    //         }
+    //     }
+    // });
 
-    $(document).on("keyup", function(event)
-    {
-        if ((keys[i].doesKeyMatchKeyEvent(event.keyCode) && (keys[i].isKeyPlaying())
-        {
-            keys[i].stopNote();
-        }
-    });
+    // $(document).on("keyup", function(event)
+    // {
+    //     if ((keys[i].doesKeyMatchKeyEvent(event.keyCode) && (keys[i].isKeyPlaying())
+    //     {
+    //         keys[i].stopNote();
+    //     }
+    // });
 });
