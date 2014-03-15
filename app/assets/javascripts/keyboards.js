53eec6c313fd6a1d4f50263e750f8a8311dbbd3d
jayrogers889csa <jayrogers889@gmail.com>
2014-03-14 20:20:06 -0500

    keyboard functionality complete

--- app/assets/javascripts/keyboards.js
+++ app/assets/javascripts/keyboards.js
@@ -1,10 +1,6 @@
-var Key = function(keycode, html_id)
-{
-    this.oscillator = context.createOscillator();
-    this.panner = context.createPanner();
-    this.delay = context.createDelayNode();
-    this.filter = context.createBiquadFilter();
-    this.volume = context.createGainNode();
+  var context = new webkitAudioContext();
+
+  var Key = function(keycode, html_id) {
     this.keycode = keycode;
     this.html_id = html_id;
     this.keyBeingPlayed = false;
@@ -14,23 +10,55 @@ var Key = function(keycode, html_id)
     this.delay_id = "delay_" + this.html_id;
     this.filter_id = "filter_" + this.html_id;
     this.volume_id = "volume_" + this.html_id;
-}
+    this.oscillator = context.createOscillator();
+    this.panner = context.createPanner();
+    this.delay = new SlapbackDelayNode(this.delay_id);
+    this.filter = context.createBiquadFilter();
+    this.volume = context.createGainNode();
+  }
 
-Key.prototype.doesKeyMatchKeyEvent = function(keyFromEvent)
-{
+  var SlapbackDelayNode = function(delay_id) {
+    //create the nodes we’ll use
+    // console.log(delay_amount);
+
+    this.input = context.createGainNode();
+    this.delay = context.createDelayNode();
+    var output = context.createGainNode(),
+        feedback = context.createGainNode(),
+        wetLevel = context.createGainNode();
+
+    //set some decent values
+    this.delay.delayTime.value = document.getElementById(delay_id).value; //150 ms delay
+    feedback.gain.value = 0.5;
+    wetLevel.gain.value = 0.5;
+
+    //set up the routing
+    this.input.connect(this.delay);
+    this.input.connect(output);
+    this.delay.connect(feedback);
+    this.delay.connect(wetLevel);
+    feedback.connect(this.delay);
+    wetLevel.connect(output);
+
+    this.connect = function(target) {
+      output.connect(target);
+    };
+
+
+  };
+
+  Key.prototype.doesKeyMatchKeyEvent = function(keyFromEvent) {
     if (this.keycode == keyFromEvent)
         return true;
 
     return false;
-}
+  }
 
-Key.prototype.isKeyPlaying = function()
-{
+  Key.prototype.isKeyPlaying = function() {
     return this.keyBeingPlayed;
-}
+  }
 
-Key.prototype.playNote = function()
-{
+  Key.prototype.playNote = function() {
     var osc = document.getElementById(this.html_id);
     $(osc).addClass('cool-border');
 
@@ -39,34 +67,33 @@ Key.prototype.playNote = function()
     this.oscillator.type = parseInt(document.getElementById(this.wave_id).value);
     this.oscillator.frequency.value = oscPitch;
     this.volume.gain.value = document.getElementById(this.volume_id).value;
-    this.filter.type = this.filter.BANDPASS
+    this.filter.type = this.filter.LOWPASS
     this.filter.detune.value = document.getElementById(this.filter_id).value;
     this.panner.setPosition(panX, 0, 0);
-
+    this.delay.delay.delayTime.value = document.getElementById(this.delay_id).value;
     this.oscillator.connect(this.panner);
     this.panner.connect(this.filter);
-    this.filter.connect(this.volume);
+    this.filter.connect(this.delay.input);
+    this.delay.connect(this.volume);
     this.volume.connect(context.destination);
 
     if (this.keyBeingPlayed != true)
-        this.oscillator.noteOn(0);
+      this.oscillator.noteOn(0);
     this.keyBeingPlayed = true;
-}
+  }
 
-Key.prototype.stopNote = function()
-{
+  Key.prototype.stopNote = function() {
     this.keyBeingPlayed = false;
 
     var osc = document.getElementById(this.html_id)
     $(osc).removeClass('cool-border');
     this.oscillator.noteOff(0);
     this.oscillator = context.createOscillator();
-}
+  }
 
-var context = new webkitAudioContext();
 
-$(document).ready(function()
-{
+
+$(document).ready(function() {
     var keys = [
         new Key(16, 1),//shift
         new Key(81, 2),//q
@@ -83,7 +110,9 @@ $(document).ready(function()
         new Key(221, 13)//]
     ]
 
-    setInterval(function(){
+    // console.log(keys);
+
+    setInterval(function() {
         for (var i = 0; i < keys.length; i++)
         {
             if (keys[i].isKeyPlaying())
@@ -91,11 +120,10 @@ $(document).ready(function()
         }
     }, 200);
 
-    $(document).on("keydown", function(event)
-    {
+    $(document).on("keydown", function(event) {
         for (var i = 0; i < keys.length; i++)
         {
-            console.log(keys[i]);
+            // console.log(keys[i]);
             if ((keys[i].doesKeyMatchKeyEvent(event.keyCode)) && (keys[i].isKeyPlaying() != true))
             {
                 keys[i].playNote();
@@ -103,8 +131,7 @@ $(document).ready(function()
         }
     });
 
-    $(document).on("keyup", function(event)
-    {
+    $(document).on("keyup", function(event) {
         for (var i = 0; i < keys.length; i++)
         {
              if ((keys[i].doesKeyMatchKeyEvent(event.keyCode)) && (keys[i].isKeyPlaying()))
@@ -113,4 +140,6 @@ $(document).ready(function()
             }
         }
     });
+
+
 });
