53a8e38fafa5d7462a909b95c3667bfaef8002b8
Tara Roys <tlroys@gmail.com>
2014-03-15 21:01:09 -0500

    Unanswered Question: it "Does the new command create a keyboard id?"

--- spec/TarasLearning/model_attributes_spec.rb
+++ spec/TarasLearning/model_attributes_spec.rb
@@ -3,5 +3,7 @@ describe "Tara Learning Stuff" do
       it "Does the new command let me add fields that are not listed in the migration? Answer: no." do 
         expect{Keyboard.new(lala:"the keyboard")}.to raise_error(ActiveRecord::UnknownAttributeError)
       end
+
+      it "Does the new command create a keyboard id?"
     end
 end
