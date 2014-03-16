a88555c142d4c2d2f13ff647c09d5e0fd9cec41c
Tara Roys <tlroys@gmail.com>
2014-03-16 15:34:31 -0500

    Key.panner is a floating point nubmer between -3 and 3
    Added test and validation to make sure panner is between -3 and 3

--- app/models/key.rb
+++ app/models/key.rb
@@ -4,4 +4,6 @@ class Key < ActiveRecord::Base
 
   validates :frequency, numericality: { greater_than_or_equal_to: 50.00, less_than_or_equal_to: 5000.00  }
   validates :wave, inclusion: { in: [0, 1, 2, 3], message: "%{value} is not a valid size" }
+  validates :panner, numericality: { greater_than_or_equal_to: -3.00, less_than_or_equal_to: 3.00  }
+
 end
