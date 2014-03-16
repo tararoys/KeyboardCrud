e7a2ba816d40fa3300b8067b7799c725b9a558a7
Tara Roys <tlroys@gmail.com>
2014-03-16 06:32:29 -0500

    Test for key has a frequency
    Actually it just tests for key has a frequency. It does not test that
    that frequency is a float.

--- spec/models/key_spec.rb
+++ spec/models/key_spec.rb
@@ -2,10 +2,16 @@ require 'spec_helper'
 
 describe Key do
 
+
   context "to have the following attributes" do
     it "has a keyboard Id and belongs to a keyboard" do
       key = Key.new(keyboard_id: 1)
       expect(key.keyboard_id).to eq(1)
+    end
+
+    it "has a frequency that is a float" do
+      key = Key.new(frequency: 1000.22)
+      expect(key.frequency).to eq(1000.22)
     end
   end
 end
