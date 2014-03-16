6873b75ff05583e9dcef5408247b6129c25bd431
Tara Roys <tlroys@gmail.com>
2014-03-16 15:58:49 -0500

    key.delay is a floating point number between 0 and 1.
    And we have the validations and tests to prove it.

--- spec/models/key_spec.rb
+++ spec/models/key_spec.rb
@@ -101,6 +101,21 @@ describe Key do
       expect(right_out_of_range.valid?).to eq(false)
      
     end
+
+    it "delay is a float between 0 and 1" do 
+      middle_c = Key.create(frequency: 261.626,  wave: 0, panner: 0, delay:    0, note_filter: 0, volume: 0.5, key_code: "15")
+      c_sharp  = Key.create(frequency: 277.183,  wave: 0, panner: 0, delay:    1, note_filter: 0, volume: 0.5, key_code: "16")
+      middle_d = Key.create(frequency: 293.665,  wave: 0, panner: 0, delay:  0.5, note_filter: 0, volume: 0.5, key_code: "17")
+      d_sharp  = Key.create(frequency: 311.127,  wave: 0, panner: 0, delay:  1.1, note_filter: 0, volume: 0.5, key_code: "18")
+      middle_e = Key.create(frequency: 329.628,  wave: 0, panner: 0, delay: -0.1, note_filter: 0, volume: 0.5, key_code: "19")
+
+      expect(middle_c.valid?).to eq(true)
+      expect(c_sharp.valid? ).to eq(true)
+      expect(middle_d.valid?).to eq(true)
+      expect(d_sharp.valid? ).to eq(false)
+      expect(middle_e.valid?).to eq(false)
+    end
+
   end
 end
 
