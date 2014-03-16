216e07ed33d71cb8c089202c8ca6190dbf0b409b
Cristin O'Connor <cristinjaneo@gmail.com>
2014-03-16 13:59:56 -0500

    working on edit keyboard view

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,9 @@ MyKeyboards::Application.routes.draw do
 
 
 
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
